e30993e1315ad12962051018e3204cfb7715ee60
Andreas Breidenthal <andreasbreidenthal@gmail.com>
2026-02-22 18:15:37 +0800

    Update nav.js

--- nav.js
+++ nav.js
@@ -7,7 +7,6 @@ document.addEventListener('DOMContentLoaded', function () {
       <a href="https://andreas-breidenthal.github.io/" class="home">Andreas Breidenthal</a>
       <div class="site-nav-links">
         <a href="index.html">Contents</a>
-        <a href="https://github.com/andreas-breidenthal/the-scalpel-slipped" target="_blank" rel="noopener">GitHub</a>
       </div>
     </nav>`;
 });
